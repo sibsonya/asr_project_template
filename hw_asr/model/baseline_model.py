68f1178507a8d38bf7becf99cf457780fb6bb642
sonya-sibagatova <sibsonya@mail.com>
2021-10-15 23:06:35 +0300

    fix baseline_model

--- hw_asr/model/baseline_model.py
+++ hw_asr/model/baseline_model.py
@@ -17,7 +17,7 @@ class BaselineModel(BaseModel):
         )
 
     def forward(self, spectrogram, *args, **kwargs):
-        return {"logits": self.net(spectrogram)}
+        return {"logits": self.net(spectrogram.transpose(1, 2))}
 
     def transform_input_lengths(self, input_lengths):
         return input_lengths  # we don't reduce time dimension here
