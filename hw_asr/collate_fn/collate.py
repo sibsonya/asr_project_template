dc8a578dfdca62e46258be55631fd7fbaeda939e
sonya-sibagatova <sibsonya@mail.com>
2021-10-15 22:58:08 +0300

    add collate_fn

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,6 +1,9 @@
 import logging
 from typing import List
 
+import torch
+import torch.nn as nn
+
 logger = logging.getLogger(__name__)
 
 
@@ -11,4 +14,21 @@ def collate_fn(dataset_items: List[dict]):
 
     result_batch = {}
     # TODO: your code here
-    raise NotImplementedError
+    # keys = ['audio', 'spectrogram', 'text_encoded', 'duration', 'text', 'audio_path']
+    # new_keys = ['audio', 'spectrogram', 'text_encoded', 'duration', 'text', 'audio_path',
+    #             'text_encoded_length', 'spectrogram_length']
+
+    for key in ['duration', 'text', 'audio_path']:
+        result_batch[key] = [item[key] for item in dataset_items]
+    
+    for key in ['audio', 'text_encoded']:
+        result_batch[key] = nn.utils.rnn.pad_sequence([item[key].squeeze() for item in dataset_items], 
+                                                      batch_first=True)
+        
+    spectograms = [item['spectrogram'].squeeze().permute(1, 0) for item in dataset_items]
+    result_batch['spectrogram'] = nn.utils.rnn.pad_sequence(spectograms, batch_first=True).permute(0, 2, 1)
+    
+    for key in ['text_encoded', 'spectrogram']:
+        result_batch[key+'_length'] = torch.tensor([value.size(-1) for item in dataset_items for k, value in item.items() if k == key])
+   
+    return result_batch
