731664870d97d352f4d81c16915e108699873b54
sonya-sibagatova <sibsonya@mail.com>
2021-10-16 17:43:19 +0300

    add gru_model.py

---
+++ hw_asr/model/gru_model.py
@@ -0,0 +1,18 @@
+from torch import nn
+
+from hw_asr.base import BaseModel
+
+
+class GRUModel(BaseModel):
+    def __init__(self, n_feats, n_class, fc_hidden=512, *args, **kwargs):
+        super().__init__(n_feats, n_class, *args, **kwargs)
+        self.encoder = nn.GRU(n_feats, fc_hidden, num_layers=2, batch_first=True)
+        self.linear = nn.Linear(fc_hidden, n_class)
+
+    def forward(self, spectrogram, *args, **kwargs):
+        output, _ = self.encoder(spectrogram.transpose(1, 2))
+        output = self.linear(output)
+        return output
+
+    def transform_input_lengths(self, input_lengths):
+        return input_lengths  # we don't reduce time dimension here
